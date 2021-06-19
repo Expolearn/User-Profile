bb80fa875339c36f57370c2fb570fa6cd22386e0
tshifhiwasinugo <84003447+tshifhiwasinugo@users.noreply.github.com>
2021-06-19 10:47:16 +0200

    address oop

---
+++ Main.js
@@ -0,0 +1,53 @@
+
+
+MyUser = {
+   Surname: "Sinugo",
+  Firstname: "Tshifhiwa ",
+  Maidenname: "Sadiki ",
+  Contact: 0762852677,
+  Education: " Univen",
+  Address: {
+    streetnum: "13333/67",
+    surbub: "Protea glen",
+    city: "Soweto",
+    country: "South Africa",
+  },
+};
+
+   var userSur = MyUser.Surname;
+console.log(userSur);
+
+var userFirst = MyUser.Firstname;
+console.log(userFirst);
+
+var userMai = MyUser. Maidenname;
+console.log(userMai);
+
+var userCon = MyUser.Contact;
+console.log(userCon);
+
+var userEdu = MyUser.Education;
+console.log(userEdu);
+
+class Address {
+
+    constructor(streetnum, surbub, city, country) {
+    this.streetnum = "13333/67";
+    this.surbub = "protea glen";
+    this.city = "soweto";
+    this.country = "south africa";
+  }
+
+MyAddress(streetn, surb, cit, coun) {
+    streetn = this.streetnum;
+    surb = this.surbub;
+    cit = this.city;
+    coun = this.country;
+    return "your address is " + streetn + " " + surb + " " + cit + " " + coun;
+  }
+
+}
+
+console.log(Address.MyAddress);
+
+
